20700efb6bc915d24b338ec3bb51376a1b1e95a5
Gu-Miao <1450206741@qq.com>
2022-07-07 10:48:29 +0800

    Add Indexes

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,6 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('events')
+@Index(['name', 'type'])
 export class EventEntity {
   @PrimaryGeneratedColumn()
   id: number;
